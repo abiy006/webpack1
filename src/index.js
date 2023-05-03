0e0c572ba89193bc88bac9dbb60c1032ee09adf8
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-05-04 00:58:55 +0300

    Liter is configured

--- src/index.js
+++ src/index.js
@@ -1,72 +1,72 @@
-import "./style.css";
+import './style.css';
 
 const arrayToDo = [
-    {
-        index: 1,
-        description: 'get up',
-        completed: true,
-    },
-    {
-        index: 2,
-        description: 'wash the dish',
-        completed: false,
-    },
-    {
-        index: 3,
-        description: 'complete To Do list project',
-        completed: false,
-    },
+  {
+    index: 1,
+    description: 'get up',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'wash the dish',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'complete To Do list project',
+    completed: false,
+  },
 ];
 
 function createMyToDo() {
-    let mytodoo = document.getElementById('mytodo');
+  const mytodoo = document.getElementById('mytodo');
 
-    let h1 = document.createElement('p');
-    h1.textContent = "Today's To Do";
-    h1.style.display = 'flex';
-    h1.style.height = '3rem';
-    h1.style.alignItems = 'center';
-    h1.style.borderBottom = '1px solid gray';
-    mytodoo.appendChild(h1);
+  const h1 = document.createElement('p');
+  h1.textContent = "Today's To Do";
+  h1.style.display = 'flex';
+  h1.style.height = '3rem';
+  h1.style.alignItems = 'center';
+  h1.style.borderBottom = '1px solid gray';
+  mytodoo.appendChild(h1);
 
-    let p1 = document.createElement('p');
-    p1.textContent = "Add to your list...";
-    p1.style.fontStyle = "italic";
-    p1.style.display = 'flex';
-    p1.style.height = '3rem';
-    p1.style.alignItems = 'center';
-    p1.style.borderBottom = '1px solid gray';
-    mytodoo.appendChild(p1);
+  const p1 = document.createElement('p');
+  p1.textContent = 'Add to your list...';
+  p1.style.fontStyle = 'italic';
+  p1.style.display = 'flex';
+  p1.style.height = '3rem';
+  p1.style.alignItems = 'center';
+  p1.style.borderBottom = '1px solid gray';
+  mytodoo.appendChild(p1);
 
-    for (let i = 0; i < arrayToDo.length; i += 1) {
-        let listItem = document.createElement('div');
-        listItem.style.display = 'flex';
-        listItem.style.height = '3rem';
-        listItem.style.alignItems = 'center';
-        listItem.style.borderBottom = '1px solid gray';
+  for (let i = 0; i < arrayToDo.length; i += 1) {
+    const listItem = document.createElement('div');
+    listItem.style.display = 'flex';
+    listItem.style.height = '3rem';
+    listItem.style.alignItems = 'center';
+    listItem.style.borderBottom = '1px solid gray';
 
-        let li = document.createElement('li');
-        li.textContent = arrayToDo[i].description;
-        
-        let mychkbox = document.createElement('input');
-        mychkbox.type = 'checkbox';
-        // mychkbox.value = 'yes';
-        mychkbox.style.marginRight = '1rem';
+    const li = document.createElement('li');
+    li.textContent = arrayToDo[i].description;
 
-        listItem.appendChild(mychkbox);
-        listItem.appendChild(li);
+    const mychkbox = document.createElement('input');
+    mychkbox.type = 'checkbox';
+    // mychkbox.value = 'yes';
+    mychkbox.style.marginRight = '1rem';
 
-        mytodoo.appendChild(listItem);
-    }
+    listItem.appendChild(mychkbox);
+    listItem.appendChild(li);
 
-    let p2 = document.createElement('p');
-    p2.textContent = "Clear all completed";
-    p2.style.display = 'flex';
-    p2.style.height = '3rem';
-    p2.style.alignItems = 'center';
-    p2.style.borderBottom = '1px solid gray';
-    p2.style.backgroundColor = '#ccc';
-    mytodoo.appendChild(p2);
+    mytodoo.appendChild(listItem);
+  }
+
+  const p2 = document.createElement('p');
+  p2.textContent = 'Clear all completed';
+  p2.style.display = 'flex';
+  p2.style.height = '3rem';
+  p2.style.alignItems = 'center';
+  p2.style.borderBottom = '1px solid gray';
+  p2.style.backgroundColor = '#ccc';
+  mytodoo.appendChild(p2);
 }
 
-createMyToDo()
+createMyToDo();
